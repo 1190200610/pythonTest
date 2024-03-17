61c0f46ed8e7e6640fff6c325da89218a9cba7dd
1190200610 <1224581500@qq.com>
2024-03-17 09:13:23 +0800

    second

--- forexInquiry.py
+++ forexInquiry.py
@@ -1,12 +1,13 @@
 import sys
 import os
+import tools
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
-def fetch_exchange_rate(date, currency_code):
+def forex_inquiry(date, name, currency_code):
     # 启动WebDriver
     driver = webdriver.Chrome()
     driver.maximize_window()
@@ -14,33 +15,32 @@ def fetch_exchange_rate(date, currency_code):
     try:
         # 模拟访问网页
         driver.get("https://www.boc.cn/sourcedb/whpj/")
-        title = driver.title
 
         # 日期
-        date = driver.find_element(by=By.id, value="erectDate")
-        date.clear()
-        date.send_keys(date)
+        date_input = driver.find_element(by=By.ID, value="erectDate")
+        date_input.clear()
+        date_input.send_keys(date)
 
         # 货币
-        currency = driver.find_element(by=By.NAME, value="pjname")
+        currency = driver.find_element(by=By.ID, value="pjname")
         currency.send_keys(currency_code)
 
         # 查询按钮
-        button = driver.find_element(by=By.XPATH, value="//input[@type='submit']")
+        button = driver.find_element(by=By.XPATH, value="/html/body/div[1]/div[5]/div[1]/div["
+                                                        "2]/div/form/div/table/tbody/tr/td[7]/input")
         button.click()
 
         # 等待页面出现, 确保存在元素可以抓取
         WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "publish")))
 
         # 找到并打印结果
-        rate = driver.find_element(by=By.XPATH, value="//table[@class='publish']/tbody/tr[1]/td[5]").text
+        rate = driver.find_element(by=By.XPATH, value="/html/body/div[1]/div[4]/table/tbody/tr[2]/td[2]").text
         print(rate)
-
         # 写入本地文件
         result_file_path = "result.txt"
         mode = "w" if not os.path.exists(result_file_path) else "a"
         with open(result_file_path, mode) as file:
-            file.write(f"Exchange rate for {date} and currency {currency_code}: {rate}")
+            file.write(f"Exchange rate for {date} and currency {name}: {rate}\n")
 
     except Exception as e:
         print(f"An error occurred: {str(e)}")
@@ -53,6 +53,7 @@ if __name__ == "__main__":
     if len(sys.argv) != 3:
         print("Usage: python3 yourcode.py <date> <currency_code>")
     else:
-        Date = sys.argv[1]
-        CurrencyCode = sys.argv[2]
-        fetch_exchange_rate(Date, CurrencyCode)
+        date = sys.argv[1]
+        currency = sys.argv[2]
+        currency_code = tools.convert(currency)
+        forex_inquiry(date, currency, currency_code)
---
+++ tools.py
@@ -0,0 +1,23 @@
+def convert(currency):
+    if currency == "USD":
+        return "美元"
+    if currency == "EUR":
+        return "欧元"
+    if currency == "GBP":
+        return "英镑"
+    if currency == "JYP":
+        return "日元"
+    if currency == "CHF":
+        return "瑞士法郎"
+    if currency == "SGD":
+        return "新加坡元"
+    if currency == "SEK":
+        return "瑞典克朗"
+    if currency == "DKK":
+        return "丹麦克朗"
+    if currency == "CAD":
+        return "加拿大元"
+    if currency == "MOP":
+        return "澳门元"
+    if currency == "PHP":
+        return "菲律宾比索"
